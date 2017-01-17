9e1f7a085b59c96c60453087c370c38d00d01058
fehmi <fehmi@FHMKRYL>
2017-01-18 00:35:02 +0300

    TickData added

---
+++ Forex.Core/Entity/Bar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Forex.Core.Entity {
+    public class Bar {
+        public double Open { get; set; }
+        public double Close { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public DateTime OpenTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public DateTime CurrentTime { get; set; }
+
+        public override string ToString() {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- Forex.Experts/TestExpert.cs
+++ Forex.Experts/TestExpert.cs
@@ -1,17 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Forex.Core.Entity;
 using Forex.Core.Manager.Order;
 using NQuotes;
 
 namespace Forex.Experts {
     public class TestExpert : MqlApi {
+        private static Bar _lastBar = new Bar();
+        private static List<TickData> _tickDatas = new List<TickData>();
         public override int start() {
-            var account = new Account(this);
-            
-            var orderManager = new OrderManager(this);
-            var result = orderManager.Buy(1, 3, 0, 0, "New Order", 0, DateTime.MaxValue);
+            //var account = new Account(this);
+
+            //var orderManager = new OrderManager(this);
+            //var result = orderManager.Buy(1, 3, 0, 0, "New Order", 0, DateTime.MaxValue);
+
+            var currentBar = new Bar {
+                Open = Open[0],
+                Close = Close[0],
+                High = High[0],
+                Low = Low[0],
+                CurrentTime = TimeCurrent(),
+                OpenTime = Time[0]
+            };
+
+            if (_lastBar.OpenTime != currentBar.OpenTime) {
+                _lastBar = currentBar;
+                _tickDatas = new List<TickData>();
+                Trace.WriteLine("New Bar Opened!");
+            }
+            else {
+                _tickDatas.Add(new TickData {
+                    CurrentTime = currentBar.CurrentTime,
+                    Price = currentBar.Close
+                });
+
+                AnalyzeTickData();
+            }
+
+
+            Trace.WriteLine(currentBar);
 
             return 0;
         }
+
+        private int AnalyzeTickData() {
+            // Todo: Linear regression must be implemented
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TickData {
+        public DateTime CurrentTime { get; set; }
+        public double Price { get; set; }
     }
 }

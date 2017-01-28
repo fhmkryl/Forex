e95ffc232282dba5eecb9a836d9dfc2d23a92c0f
fehmi <fehmi@FHMKRYL>
2017-01-28 22:38:38 +0300

    ok

---
+++ Forex.Core/Entity/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Forex.Core.Entity {
+    public class Order {
+        public double Lots { get; set; }
+        public double OpenPrice { get; set; }
+        public int Type { get; set; }
+        public DateTime OpenTime { get; set; }
+    }
+}
---
+++ Forex.Core/MathLib/MathNetSimpleRegression.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forex.Core.MathLib {
+    public static class MathNetSimpleRegression {
+        public static Tuple<double, double> Fit(List<double> xData, List<double> yData) {
+            return MathNet.Numerics.LinearRegression.SimpleRegression.Fit(xData.ToArray(), yData.ToArray());
+        }
+    }
+}
